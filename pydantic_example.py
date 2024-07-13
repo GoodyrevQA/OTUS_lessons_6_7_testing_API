6ddd092465f7ee7849b7028a2f9722fc20864423
Ivan <iegudyrev@gmail.com>
2024-07-13 15:09:27 +0300

    added hw 6-7

---
+++ pydantic_example.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, ConfigDict
+from typing import List
+
+
+class Credentials(BaseModel):
+    model_config = ConfigDict(strict=True)
+    login: str
+    pincode: int
+
+
+class UserData(BaseModel):
+    model_config = ConfigDict(strict=True)
+    data: List[Credentials]
+
+
+usr = UserData(
+    data=[
+        Credentials(login="Vasya", pincode=123),
+        Credentials(login="Prtya", pincode=456),
+    ]
+)
+
+# print(usr)
+# print(usr.model_dump())
+
+jsondict = {
+    "data": [{"login": "Vasya", "pincode": 123}, {"login": "Prtya", "pincode": 456}]
+}
+
+usr2 = UserData(**jsondict)
+# print(usr2)
